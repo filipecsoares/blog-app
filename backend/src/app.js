c151752fe5c9e278cb61306a19b51d3ff1ca3f85
Filipe Soares <filipecarlossoares@gmail.com>
2020-03-31 19:18:01 -0300

    connection to the database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,16 @@
-const express = require("express");
+const express = require('express');
 const routes = require('./routers/router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,    
+  useUnifiedTopology: true  
+  },  () => console.log('Connected to the database!'));
+
+app.use(cors());
 app.use(express.json());
 const PORT = 3333;
 
